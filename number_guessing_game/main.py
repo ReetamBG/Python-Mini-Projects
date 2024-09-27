a1c5c18b8e13c6b65a765e2934d05f5ce82a420a
ReetamBG <r33t4m@gmail.com>
2024-09-28 01:18:47 +0530

    number guessing game

--- number_guessing_game/main.py
+++ number_guessing_game/main.py
@@ -1,19 +1,43 @@
 import random 
 
 print('Enter the range of numbers')
-range = int(input('Low : ')), int(input('High : '))
+
+while(1):
+	try:
+		range = int(input('Low : ')), int(input('High : '))
+		break
+	except:
+		print('Enter a valid number')
 
 n = random.randint(range[0], range[1])
 lives = 3
 
 while(lives):
-	print('*' * 10)
+	print('*' * 20)
 	print(f'Lives Remaining : {lives}')
-	guess = int(input('Guess the number : '))
+	
+	guess = input('Guess the number (x to exit): ')
+
+	if guess.lower() == 'x':
+		print('*' * 20)
+		print('Session ended by player')
+		break
+
+	# If no input is given start over the loop else typecast input to int
+	# No input or non numeric inputs will give error when typecasting to int
+	try:
+		guess = int(guess)
+	except:
+		print('*' * 20)
+		print('Please enter a number')
+		continue
+
 	if guess == n:
+		print('*' * 20)
 		print('Yayy you won!!')
 		break;
 	lives -= 1;
 
 if lives == 0:
+	print('*' * 20)
 	print(f'Lives ended you lost\nThe correct number was {n}')
